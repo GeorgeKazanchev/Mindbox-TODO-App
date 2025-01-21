a0fd7b0acb51657b6cfdb88c6f8b9628a6285016
GeorgeKazanchev <georgekazanchev@gmail.com>
2025-01-21 20:36:10 +0300

    Выполнена вёрстка заголовка; исправлены проблемы с a11y в Firefox

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import * as styles from './Header.module.scss';
 
 export default function Header(): React.ReactNode {
-  return <h1>TODO App</h1>;
+  return (
+    <div className={styles.header}>
+      <span className={styles.preTitle}>You favorite</span>
+      <h1 className={styles.title}>TODO</h1>
+    </div>
+  );
 }
