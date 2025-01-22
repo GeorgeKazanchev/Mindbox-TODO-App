ff2939fc2644d6617238ddea594e03ba645a9715
GeorgeKazanchev <georgekazanchev@gmail.com>
2025-01-22 12:56:26 +0300

    Выполнена вёрстка поля ввода новой задачи

--- src/features/TodoInput/ui/TodoInput.tsx
+++ src/features/TodoInput/ui/TodoInput.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as styles from './TodoInput.module.scss';
 import { useAppDispatch } from '../../../shared/model/redux-hooks';
 import { addTask } from '../../../shared/model/todos-slice';
 
@@ -7,30 +8,57 @@ export default function TodoInput(): React.ReactNode {
 
   const dispatch = useAppDispatch();
 
+  const inputId = 'new-task-description';
+
+  const addNewTask = () => {
+    const task = {
+      id: '123',
+      description: taskDescription,
+      isCompleted: false,
+    };
+    dispatch(addTask(task));
+    setTaskDescription('');
+  };
+
+  const handleFormSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    addNewTask();
+  };
+
+  const handleSubmitButtonClick = (
+    evt: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    evt.preventDefault();
+    addNewTask();
+  };
+
   return (
-    <div>
-      <form
-        onSubmit={(evt: React.FormEvent<HTMLFormElement>) => {
-          evt.preventDefault();
-          const task = {
-            id: '123',
-            description: taskDescription,
-            isCompleted: false,
-          };
-          dispatch(addTask(task));
-          setTaskDescription('');
-        }}
+    <form className={styles.form} onSubmit={handleFormSubmit}>
+      <label
+        htmlFor={inputId}
+        className={styles.label}
+        aria-label="Insert new task"
       >
         <input
+          id={inputId}
+          className={styles.input}
           type="text"
-          name="description"
           value={taskDescription}
+          name="description"
           placeholder="What needs to be done?"
           onInput={(evt: React.ChangeEvent<HTMLInputElement>) => {
             setTaskDescription(evt.currentTarget.value);
           }}
         />
-      </form>
-    </div>
+      </label>
+      <button
+        style={{ display: taskDescription.length === 0 ? 'none' : 'block' }}
+        className={styles.submitButton}
+        type="submit"
+        onClick={handleSubmitButtonClick}
+      >
+        Add
+      </button>
+    </form>
   );
 }
