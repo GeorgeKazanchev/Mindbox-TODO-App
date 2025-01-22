131c8b76ced0d556f0d95153693e7f09fa452418
GeorgeKazanchev <georgekazanchev@gmail.com>
2025-01-22 15:00:11 +0300

    Исправлена ошибка

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       localsConvention: 'camelCaseOnly',
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/Mindbox-TODO/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Mindbox-TODO-App/' : '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
